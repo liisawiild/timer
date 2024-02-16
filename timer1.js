a4528332e904ddf348c7aa71224f24e3def43f4a
Lisa Wild <lisakwild@gmail.com>
2024-02-16 07:49:35 -0800

    added code to prevent edge cases

--- timer1.js
+++ timer1.js
@@ -1,22 +1,20 @@
 // Implement an alarm clock / timer which will beep after a specified amount of time has passed. 
 // The user can specify an unlimited number of alarms using command line arguments
-// 
-// Example usage:
-// node timer1.js 10 3 5 15 9 
-// This will make it beep at:
-//   3 seconds
-//   5 seconds
-//   9 seconds
-//  10 seconds
-//  15 seconds
+// Edge cases: shouldn't beep with string, - number, and no number  
+
 
 let mySchedule = process.argv.slice(2);
 
 const alarm = function(array) {
-  for (let value of array)
-  setTimeout(() => {
-    process.stdout.write(`\x07 🔔🔔🔔 it has been ${value} second(s)\n`);
-  }, value * 1000)
+  let numArr = [];
+  array.forEach((element) => numArr.push(Number(element)));
+  for (let value of numArr) {
+    if (value >= 0) {
+      setTimeout(() => {
+        process.stdout.write(`\x07🔔🔔🔔 it has been ${value} second(s)\n`);
+      }, value * 1000);
+    }
+  }
 };
 
 alarm(mySchedule);
